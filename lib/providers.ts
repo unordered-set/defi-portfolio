82f9de0df08b38686e77d934c6fbc3b3d4a84fe7
Kostyan <unordered_set@tutanota.com>
2023-08-14 22:48:02 +0000

    exit from fork

--- lib/providers.ts
+++ lib/providers.ts
@@ -2,7 +2,7 @@ import { createPublicClient, http } from "viem";
 import { mainnet } from "viem/chains";
 
 
-export const MAINNET_OVERRIDE_URL = 'https://rpc.tenderly.co/fork/760fa2ee-b81f-4f76-8fc2-2bc91f58ca88'
+export const MAINNET_OVERRIDE_URL = undefined;
 
 
 export const mainnetProvider = createPublicClient({
